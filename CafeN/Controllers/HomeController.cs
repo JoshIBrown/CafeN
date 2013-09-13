1debe1c2fc7f037267b2b8ddae0f38ea0f3cc104
Josh Brown <joshibrown@gmail.com>
2013-09-12 17:40:17 -0700

    creating project

---
+++ CafeN/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CafeN.Models;
+using WebMatrix.WebData;
+using CafeN.Filters;
+
+namespace CafeN.Controllers
+{
+    [InitializeSimpleMembership]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Location");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe))
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ CafeN/Models/LocationModels.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace CafeN.Models
+{
+    public class CafeContext : DbContext
+    {
+        public CafeContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+
+    public class Location
+    {
+        public int LocationID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+
+    public class Order
+    {
+        public Order()
+        {
+            
+        }
+
+        public Order(int LocationID)
+        {
+            this.LocationID = LocationID;
+        }
+
+        public int OrderID { get; set; }
+        public int UserID { get; set; }
+        public int LocationID { get; set; }
+        public string Size;
+        public string Bean;
+        public string Type;
+        public DateTime CreatedAt { get; set; }
+        public DateTime PickUpAt { get; set; }
+        public DateTime CancelledAt { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime CompletedAt { get; set; }
+    }
+}
